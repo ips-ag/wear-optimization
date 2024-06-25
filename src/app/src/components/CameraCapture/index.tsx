2ef094b7cb1b05f2bd82f862035112a9bde547c2
Lam Le <lam.khanh_duy_le_c@sandvik.com>
2024-06-25 17:15:05 +0700

    Update CameraCapture component to support multiple video input devices

--- src/app/src/components/CameraCapture/index.tsx
+++ src/app/src/components/CameraCapture/index.tsx
@@ -1,16 +1,27 @@
 import { Maybe } from '@/types';
 import { Button, Image, Text } from '@chakra-ui/react';
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 const videoConstraints = {
   facingMode: { exact: 'environment' },
-  width: 1280,
-  height: 720,
 };
 
 export default function CaptureImage() {
+  const [currentDeviceId, setCurrentDeviceId] = useState<string>();
   const [imageSrc, setImageSrc] = useState<Maybe<string>>(null);
+  const [, setDevices] = useState<MediaDeviceInfo[]>([]);
+
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(mediaDevices => {
+      const supportDevices = mediaDevices.filter(item => item.kind === 'videoinput');
+      console.log(supportDevices);
+      setDevices(supportDevices);
+      if (supportDevices.length > 0) {
+        setCurrentDeviceId(supportDevices[0].deviceId);
+      }
+    });
+  }, []);
 
   const webcamRef = useRef<Webcam>(null);
   const capture = useCallback(() => {
@@ -23,7 +34,14 @@ export default function CaptureImage() {
     <>
       <Image src={imageSrc || ''} />
       <Text fontSize={'xl'}>{imageSrc}</Text>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={videoConstraints} />
+      <Webcam
+        width={640}
+        height={480}
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{ ...videoConstraints, deviceId: currentDeviceId }}
+      />
       <Button onClick={capture}>Capture</Button>
     </>
   );
