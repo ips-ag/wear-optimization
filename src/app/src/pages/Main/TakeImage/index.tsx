8ea9b0e4bbca547b49390aa27e6fc8407c0b92db
Lam Le <lam.khanh_duy_le_c@sandvik.com>
2024-07-02 10:55:01 +0700

    refactor: Update TakeImage component to add image capture functionality

--- src/app/src/pages/Main/TakeImage/index.tsx
+++ src/app/src/pages/Main/TakeImage/index.tsx
@@ -1,30 +1,56 @@
-import { Box } from '@chakra-ui/react';
-import { useRef } from 'react';
+import { Maybe } from '@/types';
+import { Box, Button, HStack } from '@chakra-ui/react';
+import { useCallback, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 const videoConstraints = {
   facingMode: 'environment',
 };
 
+interface ImageActionsOverlayProps {
+  onCapture: () => void;
+}
+
+const ImageActionsOverlay = ({ onCapture }: ImageActionsOverlayProps) => {
+  return (
+    <Box
+      position="absolute"
+      bottom={0}
+      w="full"
+      h="full"
+      color="white"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <HStack spacing={4}>
+        <Button onClick={onCapture}>Take Photo</Button>
+      </HStack>
+    </Box>
+  );
+};
+
 export default function TakeImage() {
-  // const [imageSrc, setImageSrc] = useState<Maybe<string>>(null);
+  const [, setImageSrc] = useState<Maybe<string>>(null);
   const webcamRef = useRef<Webcam>(null);
 
-  // const capture = useCallback(() => {
-  //   const imageSrc = webcamRef.current?.getScreenshot?.();
-  //   setImageSrc(imageSrc);
-  // }, [webcamRef]);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot?.();
+    setImageSrc(imageSrc);
+  }, [webcamRef]);
 
   return (
-    <Box w="full" h="full">
+    <Box w="full" h="full" position={'relative'}>
       <Webcam
         width={'100%'}
         height={'100%'}
         audio={false}
         ref={webcamRef}
         screenshotFormat="image/png"
+        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
         videoConstraints={videoConstraints}
       />
+      <ImageActionsOverlay onCapture={capture} />
     </Box>
   );
 }
--- src/app/src/pages/Main/index.tsx
+++ src/app/src/pages/Main/index.tsx
@@ -1,29 +1,12 @@
 import { Box } from '@chakra-ui/react';
 import TakeImage from './TakeImage';
 
-const ImageActionsOverlay = () => {
-  return (
-    <Box
-      position="absolute"
-      bottom={0}
-      w="full"
-      h="20%"
-      bg="rgba(0,0,0,0.3)"
-      color="white"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Box>Image Actions</Box>
-    </Box>
-  );
-};
+
 
 export default function Main() {
   return (
     <Box w="full" h="full" position={'relative'}>
       <TakeImage />
-      <ImageActionsOverlay />
     </Box>
   );
 }
