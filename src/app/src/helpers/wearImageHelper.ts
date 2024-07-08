a6efab0c78f1d458d949e63eaf7fa1672da5e2b9
Lam Le <lam.khanh_duy_le_c@sandvik.com>
2024-07-08 10:34:19 +0700

    feat: Update getWearImagePath function to log wearImages for debugging

--- src/app/src/helpers/wearImageHelper.ts
+++ src/app/src/helpers/wearImageHelper.ts
@@ -3,6 +3,7 @@ import { Maybe } from '@/types';
 const wearImages = Object.values(import.meta.glob('@assets/images/wear/*.png', { eager: true, as: 'url' }));
 
 export const getWearImagePath = (type: 'photo' | 'drawing', wearCodeName: Maybe<string>) => {
+  console.log('wears', wearImages);
   if (!wearCodeName) return undefined;
 
   return wearImages.find(url => url.includes(`${wearCodeName}_${type}.png`));
