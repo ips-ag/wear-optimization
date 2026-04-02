8655a7ed67102b728e5e69d35278315f4b407238
Uroš Miletić <uros.miletic@ips-ag.com>
2026-04-02 13:21:58 +0200

    Import the assets using new Vite methods

--- src/app/src/helpers/wearImageHelper.ts
+++ src/app/src/helpers/wearImageHelper.ts
@@ -1,6 +1,6 @@
 import { Maybe } from '@/types';
 
-const wearImages = Object.values(import.meta.glob('@assets/images/wear/*.png', { eager: true, as: 'url' }));
+const wearImages = Object.values(import.meta.glob('@assets/images/wear/*.png', { eager: true, query: '?url', import: 'default' })) as string[];
 
 export const getWearImagePath = (type: 'photo' | 'drawing', wearCodeName: Maybe<string>) => {
   if (!wearCodeName) return undefined;
